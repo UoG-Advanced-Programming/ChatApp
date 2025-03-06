4dec7c6d4e7ab5468c529c4cbca395e24135508d
Arad Soutehkeshan <arad.skn84@gmail.com>
2025-03-06 18:20:19 +0000

    Add General Chat and sender

--- src/main/java/com/example/server/ChatServer.java
+++ src/main/java/com/example/server/ChatServer.java
@@ -1,17 +1,15 @@
 package com.example.server;
 
-import com.example.models.Communication;
-import com.example.models.MessageSerializer;
-import com.example.models.User;
+import com.example.models.*;
 
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.*;
 
 public class ChatServer {
+    private final GroupChat generalChat = new GroupChat("General Chat");
     private Map<User, PrintWriter> clientWriters = new ConcurrentHashMap<>();
 
     public static void main(String[] args) throws Exception {
@@ -28,6 +26,8 @@ public class ChatServer {
 
     public void addClient(User user, PrintWriter writer) {
         clientWriters.put(user, writer);
+        generalChat.addParticipant(user);
+        send(user, new SystemMessage(SystemMessageType.CHAT_INIT,  generalChat));
     }
 
     public void removeClient(User user) {
@@ -36,5 +36,12 @@ public class ChatServer {
 
     public PrintWriter getClient(User user) {
         return clientWriters.get(user);
+    }
+
+    private void send(User user, Communication message) {
+        PrintWriter writer = clientWriters.get(user);
+        if (writer != null) {
+            writer.println(MessageSerializer.serialize(message));
+        }
     }
 }
