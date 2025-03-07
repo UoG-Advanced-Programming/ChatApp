d1b185887ed2aad684f19f309c61788e46958f69
Arad Soutehkeshan <arad.skn84@gmail.com>
2025-03-07 20:37:46 +0000

    Use the client side message processing factory

--- src/main/java/com/example/client/ClientHandler.java
+++ src/main/java/com/example/client/ClientHandler.java
@@ -2,7 +2,6 @@ package com.example.client;
 
 import com.example.models.*;
 
-import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 
@@ -31,26 +30,7 @@ public class ClientHandler implements Runnable {
 
     public void processMessage(String jsonMessage) {
         Communication message = MessageSerializer.deserialize(jsonMessage);
-        switch (message.getType()) {
-            case TEXT:
-                TextMessage textMessage = (TextMessage) message;
-                // gui.showMessage(textMessage);
-                break;
-
-            case USER_UPDATE:
-                UserUpdateMessage userUpdateMessage = (UserUpdateMessage) message;
-                System.out.println("User " + userUpdateMessage.getUser().getUsername() + " is now " + userUpdateMessage.getStatus());
-                break;
-
-            case SYSTEM:
-                SystemMessage systemMessage = (SystemMessage) message;
-                Chat chat = systemMessage.getChat();
-                SwingUtilities.invokeLater(() -> {
-                    if (!gui.hasChat(chat)) {
-                        gui.addChat(chat);
-                    }
-                });
-                break;
-        }
+        ClientMessageProcessor processor = ClientMessageProcessorFactory.getProcessor(message.getType());
+        processor.processMessage(message, gui);
     }
 }
