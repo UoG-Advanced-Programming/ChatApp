8d8bc203b8c57ca2c007ead09851bc0795d63250
Arad Soutehkeshan <arad.skn84@gmail.com>
2025-03-08 17:46:39 +0000

    Handle General Chat creation

--- src/main/java/com/example/client/processing/ClientSystemMessageProcessor.java
+++ src/main/java/com/example/client/processing/ClientSystemMessageProcessor.java
@@ -2,10 +2,13 @@ package com.example.client.processing;
 
 import com.example.client.gui.ClientGUI;
 import com.example.common.chats.Chat;
+import com.example.common.chats.GroupChat;
 import com.example.common.messages.Communication;
 import com.example.common.messages.SystemMessage;
+import com.example.common.users.User;
 
 import javax.swing.*;
+import java.util.Set;
 
 public class ClientSystemMessageProcessor extends ClientMessageProcessor {
     @Override
@@ -17,5 +20,15 @@ public class ClientSystemMessageProcessor extends ClientMessageProcessor {
                 gui.addChat(chat);
             }
         });
+
+        // If the chat is the General Chat, add its participants to the active_users list
+        if (chat instanceof GroupChat && chat.getName().equals("General Chat")) {
+            Set<User> participants = chat.getParticipants();
+            SwingUtilities.invokeLater(() -> {
+                for (User participant : participants) {
+                    gui.addActiveUser(participant); // Add each participant to the active_users list
+                }
+            });
+        }
     }
 }
